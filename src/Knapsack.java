2bab3c79e36869408d9c4ff14d3c6d8e56e419d4
rois <roi.schmidt77@gmail.com>
2020-02-13 20:44:35 +0200

    + recursive solution

--- src/Knapsack.java
+++ src/Knapsack.java
@@ -31,7 +31,7 @@ public class Knapsack {
         int maxValue = 0;
         for(int i = 0; i < items.size(); i++) {
             Item item = items.get(i);
-            if(item.w < maxWeight) {
+            if(item.w <= maxWeight) {
                 sumList.add(new Item(item.w, item.v));
                 if (item.v > maxValue)
                     maxValue = item.v;
@@ -39,7 +39,7 @@ public class Knapsack {
                 for (int j = 0; j < sumListSize; j++) {
                     if (j == i) continue;
                     Item sumItem = sumList.get(j);
-                    if (sumItem.w + item.w < maxWeight) {
+                    if (sumItem.w + item.w <= maxWeight) {
                         sumList.add(new Item(sumItem.w + item.w, sumItem.v + item.v));
                         if (sumItem.v + item.v > maxValue)
                             maxValue = sumItem.v + item.v;
@@ -53,8 +53,24 @@ public class Knapsack {
     public static void main(String[] args) {
         Knapsack k = new Knapsack();
         List<Item> input = Arrays.asList(new Item(1, 6), new Item(2, 10), new Item(3, 12));
-        System.out.println(k.knapsack(input, 5));
-        System.out.println("knapsack(input,5) = 22 : " + (k.knapsack(input, 5) == 18));
+        System.out.println("knapsack(input,5) = 22 : " + (k.knapsack(input, 5) == knapsackRec(input,5)));
 
+    }
+
+    /**
+     * recursive solution
+     */
+    public static int knapsackRec(List<Item> items, int maxWeight){
+        return knapsackRecImpl(items,0,maxWeight,0);
+    }
+
+    public static int knapsackRecImpl(List<Item> items, int heightSum,int maxHeight,int idx)  {
+        if(idx == items.size())
+            return 0;
+        Item item = items.get(idx);
+        if(item.w + heightSum > maxHeight)
+            return knapsackRecImpl(items,heightSum,maxHeight,idx+1);
+        return Math.max(knapsackRecImpl(items,heightSum + item.w ,maxHeight,idx+1) + item.v,
+                knapsackRecImpl(items,heightSum,maxHeight,idx+1));
     }
 }
